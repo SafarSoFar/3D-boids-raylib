57d16d189cfc9570d599b121a3801022fa6a0a48
SafarSoFar <cafar0525@gmail.com>
2024-08-18 12:43:39 +0500

    refactoring, avoidance logic

--- game/app_cpp/main.cpp
+++ game/app_cpp/main.cpp
@@ -5,33 +5,79 @@
 #include <random>
 using namespace std;
 
+bool operator==(Vector3 lhs,Vector3 rhs){
+    return lhs.x == rhs.x 
+    && lhs.y == rhs.y 
+    && lhs.z == rhs.z; 
+}
+
+bool operator!=(Vector3 lhs, Vector3 rhs){
+    return !(lhs == rhs);
+}
+
+class Boid;
+
+float g_boidRadius = 0.5f;
+float g_boidDestinationLimit = 15.0f;
+float g_boidSeparationRadius = 1.5f;
+float g_boidAvoidanceSpeed = 1.5f;
+const int g_boidsAmount = 100;
+vector<Boid> g_boids;
+
+std::random_device rd;
+std::default_random_engine eng(rd());
+std::uniform_real_distribution<float> distr(-g_boidDestinationLimit,g_boidDestinationLimit);  
 
 class Boid{
 	public:
 		Vector3 pos;
 		Vector3 rotation;
-		void Move(){
-            if(this->pos.x == this->destination.x && this->pos.y == this->destination.y && this->pos.z == this->destination.z){
-                std::random_device rd;
-                std::default_random_engine eng(rd());
-                std::uniform_real_distribution<float> distr(-5.0f,5.0f);  
-                this->destination = Vector3{distr(eng), distr(eng), distr(eng)};
-            }
-            this->pos = Vector3MoveTowards(this->pos, this->destination, speed);
-		}
+        void Behave(){
+            Avoid();
+            Move();
+
+        }
 		Boid(){
-			this->pos = Vector3Zero();
+			this->pos = Vector3{distr(eng), distr(eng), distr(eng)};
 			this->rotation = Vector3Zero();
-			this->destination = Vector3Zero();
+			this->destination = Vector3{distr(eng), distr(eng), distr(eng)};
 		}
 	private:
+        void Move(){
+            this->pos = Vector3MoveTowards(this->pos, this->destination, speed);
+        }
+        bool Avoid(){
+            bool isAvoiding = false;
+            for(int i = 0; i < g_boidsAmount; i++){
+                if(g_boids[i].pos != this->pos){
+                    if(CheckCollisionSpheres(this->pos, g_boidSeparationRadius, g_boids[i].pos, g_boidSeparationRadius)){
+                        if(!isAvoiding){
+                            isAvoiding = true;
+                            this->destination = Vector3Zero();
+                        }
+                        this->destination.x += (this->pos.x - g_boids[i].pos.x) * 4; 
+                        this->destination.y += (this->pos.y - g_boids[i].pos.y) * 4; 
+                        this->destination.z += (this->pos.z - g_boids[i].pos.z) * 4; 
+                    }                   
+                }
+            }
+            this->pos = Vector3MoveTowards(this->pos, this->destination, speed*g_boidAvoidanceSpeed);
+
+            return isAvoiding;
+        }
 		Vector3 destination;
-        float speed = 1.0f;
+        float speed = 0.2f;
 };
+
+
 
 
 int main ()
 {
+    for(int i = 0; i < g_boidsAmount; i++){
+        Boid boid = Boid();
+        g_boids.push_back(boid);
+    }
 	const int screenWidth = 800;
     const int screenHeight = 640;
 
@@ -48,8 +94,6 @@ int main ()
     DisableCursor();
     SetTargetFPS(60);  
 
-    Boid boid = Boid();
-
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
 
@@ -61,9 +105,11 @@ int main ()
             ClearBackground(RAYWHITE);
 
             BeginMode3D(camera);
-
-			DrawCube(boid.pos, 5.0f,5.0f,5.0f, BLACK);
-            boid.Move();
+            DrawPlane({0.0f,-8.0f,0.0f}, {30.0f,30.0f}, BLACK);
+            for(int i = 0; i < g_boidsAmount; i++){
+			    DrawSphere(g_boids[i].pos, g_boidRadius, PURPLE);
+                g_boids[i].Behave();
+            }
             EndMode3D();
 
 
